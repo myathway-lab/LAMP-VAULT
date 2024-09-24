054ae83fea120d56704992be1ddec6931ba2fa0f
myathway.lab <myathway.lab@gmail.com>
2024-09-25 05:01:07 +0800

    AWS VPC Subnet NAT IGW Route Creation

---
+++ Create-VPC-SG/main.tf
@@ -0,0 +1,128 @@
+data "aws_availability_zones" "azs" {
+  state = "available"
+}
+
+##################################################
+# VPC
+##################################################
+
+resource "aws_vpc" "main" {
+  cidr_block           = var.cidr
+  instance_tenancy     = var.instance_tenancy
+  enable_dns_hostnames = var.enable_dns_hostnames
+  enable_dns_support   = var.enable_dns_support
+
+  tags = merge(
+    { "Name" = var.name },
+    var.tags
+  )
+}
+
+################################################################################
+# Publiс Subnet For Web Servers
+################################################################################
+
+resource "aws_subnet" "public" {
+#  count                   = local.len_public_subnets
+  vpc_id                  = aws_vpc.main.id
+  availability_zone       = data.aws_availability_zones.azs.names[0]
+  cidr_block              = var.public_subnets
+  map_public_ip_on_launch = var.map_public_ip_on_launch
+  tags = {
+    Name = "Pub-Subnet-Web"
+  }  
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "RouteTable-Web"
+  }  
+}
+
+resource "aws_route_table_association" "public" {
+#  count          = local.len_public_subnets
+  route_table_id = aws_route_table.public.id
+  subnet_id      = aws_subnet.public.subnet_id
+
+}
+
+resource "aws_route" "public_internet_gateway" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.this.id
+  timeouts {
+    create = "5m"
+  }
+}
+
+################################################################################
+# Private Subnets for DB Servers
+################################################################################
+
+resource "aws_subnet" "private" {
+#  count             = local.len_private_subnets
+  vpc_id            = aws_vpc.main.id
+  availability_zone = data.aws_availability_zones.azs.names[1]
+  cidr_block        = var.private_subnets
+  tags = {
+    Name = "Pri-Subnet-DB"
+  }  
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "DB-RouteTable"
+  }
+}
+
+resource "aws_route_table_association" "private" {
+#  count          = local.len_private_subnets
+  route_table_id = aws_route_table.private.id
+  subnet_id      = aws_subnet.public.subnet_id
+}
+
+resource "aws_route" "private_nat_gateway" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = var.nat_gateway_destination_cidr_block
+  nat_gateway_id         = aws_nat_gateway.nat.id
+  timeouts {
+    create = "5m"
+  }
+}
+
+
+################################################################################
+# Internet Gateway
+################################################################################
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.main.id
+  tags = merge(
+    { "Name" = var.name },
+    var.tags,
+  )
+}
+
+################################################################################
+# NAT Gateway
+################################################################################
+
+resource "aws_eip" "nat" {
+  domain = "vpc"
+  tags = {
+    Name = var.tags
+  }
+  depends_on = [aws_internet_gateway.this]
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id = aws_subnet.public.subnet_id
+  depends_on = [aws_internet_gateway.this]
+  tags = {
+    Name = "LAMP NAT"
+  }
+}
+
---
+++ Create-VPC-SG/variables.tf
@@ -0,0 +1,152 @@
+##################################################
+# VPC
+##################################################
+variable "name" {
+  description = "(Require) Name to be used on all the resources as identifier"
+  type        = string
+  default     = "VPC-LAMP"
+}
+
+variable "cidr" {
+  description = "(Optional) The IPv4 CIDR block for the VPC. CIDR can be explicitly set"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "instance_tenancy" {
+  description = "(Optional) A tenancy option for instances launched into the VPC"
+  type        = string
+  default     = "default"
+}
+
+variable "azs" {
+  description = "(Require) A list of availability zones names or ids in the region"
+  type        = list(string)
+  default     = []
+}
+
+variable "enable_dns_hostnames" {
+  description = "Should be true to enable DNS hostnames Default: `true`"
+  type        = bool
+  default     = true
+}
+
+variable "enable_dns_support" {
+  description = "Should be true to enable DNS support Default: `true`"
+  type        = bool
+  default     = true
+}
+
+
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  type        = map(string)
+  default     = {
+    "Environment" = "Production"
+    "Project"     = "TFC-VAULT-AWS-Project"
+    "Owner"       = "MyaThway"
+  }
+}
+
+################################################################################
+# Publiс Subnets for Web Servers
+################################################################################
+
+variable "public_subnets" {
+  description = "A list of public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24"]
+}
+
+variable "create_multiple_public_route_tables" {
+  description = "Indicates whether to create a separate route table for each public subnet. Default: `false`"
+  type        = bool
+  default     = false
+}
+
+variable "map_public_ip_on_launch" {
+  description = "Specify true to indicate that instances launched into the subnet should be assigned a public IP address. Default is `false`"
+  type        = bool
+  default     = true
+}
+
+variable "public_subnet_names" {
+  description = "Explicit values to use in the Name tag on public subnets."
+  type        = list(string)
+  default     = ["Pub-Subnet-WebServers"]
+}
+
+#variable "public_subnet_suffix" {
+#  description = "Suffix to append to public subnets name"
+#  type        = string
+#  default     = "public"
+#}
+
+
+
+################################################################################
+# Private Subnets for DB Servers
+################################################################################
+
+variable "private_subnets" {
+  description = "A list of private subnets"
+  type        = list(string)
+  default     = ["10.0.2.0/24"]
+}
+
+variable "private_subnet_names" {
+  description = "Explicit values to use in the Name tag on private subnets."
+  type        = list(string)
+  default     = ["Pri-Subnet-WebServers"]
+}
+
+#variable "private_subnet_suffix" {
+#  description = "Suffix to append to private subnets name"
+#  type        = string
+#  default     = "private"
+#}
+
+
+################################################################################
+# NAT Gateway
+################################################################################
+
+variable "nat_gateway_destination_cidr_block" {
+  description = "Used to pass a custom destination route for private NAT Gateway. If not specified, the default 0.0.0.0/0 is used as a destination route"
+  type        = string
+  default     = "0.0.0.0/0"
+}
+
+##############################################
+#Vault
+##############################################
+
+variable "aws_secret_backend_path" {
+  description = "aws_secret_backend_path"
+  type        = string
+  default = "aws-master-account"
+}
+
+variable "aws_netadmin_role_name" {
+  description = "aws_secret_backend_path"
+  type        = string
+  default = "master-networkadmin-role"
+}
+
+
+variable "backend_path" {
+    description = "Vault aws secret path"
+    type = string
+    default = "aws-master-account"
+}
+
+variable "vault_dynamic_role" {
+    description = "Vault dynamic role"
+    type = string
+    default = "master-iamadmin-role"
+}
+
+variable "aws_region" {
+  description = "AWS Region for Vault AWS Secret Role"
+  default     = "ap-southeast-1"
+}
